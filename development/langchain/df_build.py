ec12daa76444e58406ee03d2567e83129d724217
William Lindskog <w.lindskog@CRD-ITS3.dnde.emea.denso>
2023-07-27 16:47:46 +0200

    Development script

---
+++ development/langchain/df_build.py
@@ -0,0 +1,11 @@
+# File: db_build.py
+
+# Imports 
+import logging
+
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+
+logging.INFO('Packages imported. ')
